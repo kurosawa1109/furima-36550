c8698b7f6b95782f46c200f75659ab76ad155e85
kurosawa1109 <88430509+kurosawa1109@users.noreply.github.com>
2021-09-03 16:10:08 +0900

    userモデルテストコード

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,13 +4,20 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname,         presence: true
-  validates :family_name,      presence: true
-  validates :first_name,       presence: true
-  validates :family_name_kana, presence: true
-  validates :first_name_kana,  presence: true
-  validates :birthday,         presence: true
+  validates :nickname, presence: true
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ } do
+    validates :family_name
+    validates :first_name
+  end
+
+  with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/ } do
+    validates :family_name_kana
+    validates :first_name_kana
+  end
 
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates :password, format: { with: VALID_PASSWORD_REGEX}
+
+  validates :birthday, presence: true
 end
