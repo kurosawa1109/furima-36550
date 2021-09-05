1da5c01fd2846a454840eb08bfd6197d63a66c22
kurosawa1109 <88430509+kurosawa1109@users.noreply.github.com>
2021-09-05 14:24:10 +0900

    テストコード

--- app/models/item.rb
+++ app/models/item.rb
@@ -21,6 +21,6 @@ class Item < ApplicationRecord
      validates :days_id
    end
     validates :price, numericality: { with: /\A[0-9]+\z/}
-    validates :price,numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: "Can't be blunk" }
+    validates :price,numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999}
   end
 end
