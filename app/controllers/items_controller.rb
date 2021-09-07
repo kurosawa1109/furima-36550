8c7d7503bf413184ea924b7466c6aca07d7bb42f
kurosawa1109 <88430509+kurosawa1109@users.noreply.github.com>
2021-09-07 18:49:25 +0900

    修正２

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -35,8 +35,10 @@ class ItemsController < ApplicationController
   end
 
   def destroy
+    if @item.user_id == current_user.id
     @item.destroy
     redirect_to root_path
+    end
   end
 
   private
