8ab3c873c5ccf00577ddba79860ba7483c0f1e3a
kurosawa1109 <88430509+kurosawa1109@users.noreply.github.com>
2021-09-10 20:30:30 +0900

    修正１

--- spec/factories/buy_shipping_addresses.rb
+++ spec/factories/buy_shipping_addresses.rb
@@ -6,7 +6,6 @@ FactoryBot.define do
     house_number  { '1-1' }
     building      { 'ボロビル' }
     phone_number  { '07014823455' }
-    association :item
     token { 'tok_abcdefghijk00000000000000000' }
   end
 end
