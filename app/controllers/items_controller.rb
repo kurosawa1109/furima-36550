5b167f75af609c51b785f2877c544f7898e333b9
kurosawa1109 <88430509+kurosawa1109@users.noreply.github.com>
2021-09-10 17:17:59 +0900

    環境変数の設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,8 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :set_item, except: [:index, :new, :create]
-  before_action :move_to_index, only: [:edit, :destroy]
+  before_action :move_to_index, only: [:edit, :update, :destroy]
+  before_action :redirect_root, only: [:edit, :update]
 
   def index
     @items = Item.all.order('id DESC')
@@ -52,5 +53,11 @@ class ItemsController < ApplicationController
 
   def move_to_index
     redirect_to action: :index unless current_user.id == @item.user_id
+  end 
+
+  def redirect_root
+    if current_user.id == @item.user_id && @item.buy.present?
+      redirect_to action: :index 
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root to: 'items#index'
-  resources :items
+  resources :items do
+    resources :buys, only: [:index, :create]
+  end
   
   devise_for :users
 end
